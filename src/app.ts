4212054fbe01a61ff8d6229deea099fb5a33b186
Barry G Becker <barrybecker4@gmail.com>
2025-06-19 06:27:38 -0700

    Phase 5: deploy

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-// Worker entry point
-import { WebSocketHibernationServer } from './lib/server/websocket.ts';
-
-// Export the Durable Object class
-export { WebSocketHibernationServer };
-
-// The main SvelteKit app will be handled by the adapter
-export { default } from './handler.js';
